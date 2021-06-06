2fc160ca3f47276555b623296f547f84c43efcbc
Christopher DiStasio <cadistasio@gmail.com>
2021-06-06 03:35:17 +0000

    Adds JSON objects and task list

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 
+let gondola = {quantity: 7, insured: true, operational: true, pricePer: 3800};
+let stockCar = {quantity: 1, insured: true, operational: false, pricePer: 6500};
+let boxCar = {quantity: 8, insured: true, operational: true, pricePer: 4157};
+let tankCar = {quantity: 3, insured: true, operational: true, pricePer: 7200};
+
+
+//Task 1: Get list of all cars reported as non operational (operational = false)
+
+
+
+
+//Task 2: Get count of all cars with no Insurance
+
+
+
+
+//Task 3: Total dollar amount spent on tankCars, stockCars, boxCars over 6500.00
+// if the dollar amount spent on each ^ > 6500.00 add to total
+
+
+
+
+//Task 4: Output results
+
+
 
 
 
