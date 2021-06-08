548d8a272b9ab350f76e44da5f302d50a0920f84
Christopher DiStasio <cadistasio@gmail.com>
2021-06-07 23:21:10 -0500

    Completes Task 2 lesson version

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let gondola = {quantity: 7, insured: true, operational: true, pricePer: 3800};
 let stockCar = {quantity: 1, insured: true, operational: false, pricePer: 6500};
 let boxCar = {quantity: 8, insured: true, operational: true, pricePer: 4157};
@@ -7,12 +6,67 @@ let tankCar = {quantity: 3, insured: true, operational: true, pricePer: 7200};
 
 //Task 1: Get list of all cars reported as non operational (operational = false)
 
+let allCars = [];
 
+allCars.push(gondola, stockCar, boxCar, tankCar);
+
+let notOperational = [];
+
+//For lesson version
+
+if (!gondola.operational){
+  notOperational.push(gondola);
+}
+
+if (!stockCar.operational){
+  notOperational.push(stockCar);
+}
+
+if (!boxCar.operational){
+  notOperational.push(boxCar);
+}
+
+if (!tankCar.operational){
+  notOperational.push(tankCar);
+}
+
+//Stubbing refactoring
+
+// for (let i = 0; i < allCars.length; i++){
+//  for (const car of allCars){
+//    if (car.operational === false){
+//      notOperational.push(car)
+//    }
+//  }
+// }
+
+console.log(notOperational);
 
 
 //Task 2: Get count of all cars with no Insurance
 
+let uninsured = 0;
 
+if (!gondola.insured){
+  uninsured++;
+}
+
+if (!stockCar.insured){
+  uninsured++;
+}
+
+if (!boxCar.insured){
+  uninsured++;
+}
+
+if (!tankCar.insured){
+  uninsured++;
+}
+
+console.log(uninsured);
+
+
+//Refactor Task 2
 
 
 //Task 3: Total dollar amount spent on tankCars, stockCars, boxCars over 6500.00
@@ -22,8 +76,3 @@ let tankCar = {quantity: 3, insured: true, operational: true, pricePer: 7200};
 
 
 //Task 4: Output results
-
-
-
-
-
